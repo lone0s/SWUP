38bee4aed674140a3893b3323a20002e1c8a683e
Baptiste Saint-Léger <baptiste.sl2000@gmail.com>
2023-04-25 14:07:02 +0200

    update btn view

---
+++ Assets/View_script.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class View_script : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+        AddPrefab("Onglet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void AddPrefab(string name){
+       
+        // Instancie le prefab à la position de l'objet courant
+        GameObject newButton = Instantiate(prefab, transform.position, Quaternion.identity);
+
+        
+        // Met à jour le texte du bouton
+        Text buttonText = newButton.GetComponentInChildren<Text>();
+        buttonText.text = name;
+        
+        // Ajoute le nouvel objet à la hiérarchie en tant qu'enfant de l'objet courant
+        newButton.transform.parent = transform;
+    }
+}
