b2063d022ab28ffc7efd74c0c0038e7c0437b155
Baptiste Saint-Léger <baptiste.sl2000@gmail.com>
2023-04-25 14:47:32 +0200

    release prefab

--- Assets/View_btn.cs
+++ Assets/View_btn.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class View_btn : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button deleteButton = this.transform.Find("Close_btn").GetComponent<Button>();
+        deleteButton.onClick.AddListener(() => Destroy(gameObject));
     }
 
     // Update is called once per frame
