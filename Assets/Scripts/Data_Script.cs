4246bb4dad4e616c5a83c04f98319672c74cc2da
lone0$p <loneos1337@gmail.com>
2023-04-28 15:42:19 +0200

    added 1D/3D input prefab

---
+++ Assets/Scripts/Data_Script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Xml;
+
+public class Data_Script : MonoBehaviour
+{
+    //Position, vitesse Rotation, Taille 
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
